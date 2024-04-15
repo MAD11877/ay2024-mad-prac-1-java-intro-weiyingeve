1f201a6a78482c787af1d2df11608e36b4887119
weiyingeve <148025168+weiyingeve@users.noreply.github.com>
2024-04-15 14:46:50 +0800

    edited

--- Question2.java
+++ Question2.java
@@ -16,6 +16,6 @@ public class Question2
     double weight = in.nextDouble();
 
     double bmi = weight / (height * height);
-    System.out.println("BMI: " + bmi);
+    System.out.println(bmi);
   }
 }
